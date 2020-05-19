a052d6bda1a291a317d01f78e57e88902964a1b3
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-05-19 13:57:28 +0200

    Feat(Matricula): Crear entidad clase Matricula + ManyToOne + test OK

--- src/main/java/org/formacio/setmana2/domini/Matricula.java
+++ src/main/java/org/formacio/setmana2/domini/Matricula.java
@@ -1,12 +1,28 @@
 package org.formacio.setmana2.domini;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="T_MATRICULES")
 public class Matricula {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="MAT_ID")
 	private Long id; 
 	
+	@ManyToOne
+	@JoinColumn(name="MAT_ALUMNE")
 	private Alumne alumne;
 	
+	@ManyToOne
+	@JoinColumn(name="MAT_CURS")
 	private Curs curs;
 	
 	public Long getId() {
